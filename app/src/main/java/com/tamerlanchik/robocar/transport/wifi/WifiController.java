7d49cddf659e3cbf0c7c97d2ffedcf79248875be
tamerlanchik <a.kochnov@internet.ru>
2022-11-13 23:28:05 +0300

    Incapsulate UDP and TCP sockets usage in one class UnionSocket

--- app/src/main/java/com/tamerlanchik/robocar/transport/wifi/WifiController.java
+++ app/src/main/java/com/tamerlanchik/robocar/transport/wifi/WifiController.java
@@ -21,6 +21,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.List;
 
@@ -30,10 +31,8 @@ public class WifiController implements Communicator {
     MutableLiveData<Event> mEventLiveData = new MutableLiveData<>();
     Statistics mSendStatistics = new Statistics();
 
-    Socket mSocket;
-    DatagramSocket mUdpSocket;
+    UnionSocket mUSocket;
     String mDestinationHost;
-    InetAddress mDestinationHostAddress;
     int mDestinationPort;
     Thread mListeningThread;
     boolean mSelectUdp;
@@ -70,83 +69,32 @@ public class WifiController implements Communicator {
 
     }
 
-    public boolean maintainTCPSocket() {
-        if (mSocket == null) {
-            mSocket = new Socket();
-        }
-        if (!mSocket.isConnected()) {
-            long start = System.currentTimeMillis();
-            try {
-//                mSocket = new Socket(mDestinationHost, mDestinationPort);
-//                mSocket.connect(new InetSocketAddress(mDestinationHost, mDestinationPort), 100);
-                mSocket.connect(new InetSocketAddress(mDestinationHost, mDestinationPort));
-                return true;
-            } catch (Exception e) {
-                handleError(new Exception("Cannot create ping socket"));
-                Log.e(TAG, "Cannot create ping socket: " + (System.currentTimeMillis() - start));
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean maintainUPDSocket() throws SocketException {
-        if (mUdpSocket == null) {
-            mUdpSocket = new DatagramSocket(mDestinationPort);
-        }
-        if (!mUdpSocket.isConnected()) {
-            long start = System.currentTimeMillis();
-            try {
-                mDestinationHostAddress = InetAddress.getByName(mDestinationHost);
-//                mUdpSocket.connect(address, mDestinationPort);
-//                return true;
-            } catch (Exception e) {
-                handleError(new Exception("Cannot create ping socket"));
-                Log.e(TAG, "Cannot create ping socket: " + (System.currentTimeMillis() - start));
-                return false;
-            }
-        }
-        return true;
-    }
     public boolean maintainSocket() {
-        boolean res;
-        if (mSelectUdp) {
+        if (mUSocket == null) {
             try {
-                res = maintainUPDSocket();
+                mUSocket = mSelectUdp
+                        ? UnionSocket.createUDP(mDestinationHost, mDestinationPort)
+                        : UnionSocket.createTCP(mDestinationHost, mDestinationPort);
+                mUSocket.connect();
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
+                return false;
             }
-        } else {
-            res = maintainTCPSocket();
-        }
-        if (!res) {
-            return res; // TODO
-        }
 //        if (mListeningThread != null) {
 //            mListeningThread.interrupt();
 //        }
 
-        if (mListeningThread == null) {
-
-            if (mSelectUdp) {
-                mListeningThread = new Thread(()->{
-                    listenUdp();
-                });
-            } else {
-                mListeningThread = new Thread(()->{
-                    listen();
-                });
+            if (mListeningThread == null) {
+                mListeningThread = new Thread(this::listen);
+                mListeningThread.setDaemon(true);
+                mListeningThread.start();
             }
-
-            mListeningThread.setDaemon(true);
-            mListeningThread.start();
         }
-
         return true;
     }
 
     @Override
-    public boolean send(Package pkg) {
+    public boolean send (Package pkg){
         if (!maintainSocket()) {
             return false;
         }
@@ -154,69 +102,31 @@ public class WifiController implements Communicator {
             ByteBuffer packed = ByteStuffingPackager.packWithByteStuffing(
                     ByteBuffer.wrap(pkg.getBinary())
             );
-            if (mSelectUdp) {
-                byte[] buff = packed.array();
-                DatagramPacket packet = new DatagramPacket(buff, buff.length, mDestinationHostAddress, mDestinationPort);
-                mUdpSocket.send(packet);
-            } else {
-                mSocket.getOutputStream().write(packed.array());
-                mSocket.getOutputStream().flush();
-            }
+            mUSocket.send(packed.array());
         } catch (IOException e) {
             handleError(new Exception("Cannot write ping to socket"));
             Log.e(TAG, "Cannot write ping to socket");
-            mSocket = null;
-            mUdpSocket = null;
+            mUSocket = null;
             return false;
         }
         return true;
     }
 
-    private void listenUdp() {
-        while (mUdpSocket != null) {
+    private void listen () {
+        while (mUSocket != null && mUSocket.isActive()) {
             Log.d(TAG, "Client connected");
-            ByteStuffingPackager packager = new ByteStuffingPackager();
-            byte[] buffer = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            int bytesRead = 0;
-            while (bytesRead != -1) {
-                try {
-                    mUdpSocket.receive(packet);
-                    bytesRead = packet.getLength();
-                } catch (IOException e) {
-                    Log.e(TAG, e.getMessage());
-                    bytesRead = -1;
-                }
-                if (bytesRead > 0) {
-                    List<ByteBuffer> res = packager.unpackWithByteStuffing(ByteBuffer.wrap(buffer, 0, bytesRead));
-                    if (res != null && res.size() > 0) {
-                        Log.d(TAG, "Got new answer");
-                        for(ByteBuffer frame : res) {
-                            handleNewFrame(frame);
-                        }
-                    }
-                }
-            }
-            Log.d(TAG, "Client disconnected");
-        }
-    }
-
-    private void listen() {
-        InputStream in;
-        while (mSocket != null && mSocket.isConnected()) {
             try {
-                in = mSocket.getInputStream();
+                mUSocket.newClient();
             } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
-                return;
+                e.printStackTrace();
+                continue;
             }
-            Log.d(TAG, "Client connected");
             ByteStuffingPackager packager = new ByteStuffingPackager();
             byte[] buffer = new byte[1024];
             int bytesRead = 0;
             while (bytesRead != -1) {
                 try {
-                    bytesRead = in.read(buffer);
+                    bytesRead = mUSocket.receive(buffer);
                 } catch (IOException e) {
                     Log.e(TAG, e.getMessage());
                     bytesRead = -1;
@@ -225,7 +135,7 @@ public class WifiController implements Communicator {
                     List<ByteBuffer> res = packager.unpackWithByteStuffing(ByteBuffer.wrap(buffer, 0, bytesRead));
                     if (res != null && res.size() > 0) {
                         Log.d(TAG, "Got new answer");
-                        for(ByteBuffer frame : res) {
+                        for (ByteBuffer frame : res) {
                             handleNewFrame(frame);
                         }
                     }
@@ -235,27 +145,27 @@ public class WifiController implements Communicator {
         }
     }
 
-    private void handleNewFrame(ByteBuffer frame) {
+    private void handleNewFrame (ByteBuffer frame){
         Package pkg = new Package(frame.array(), frame.position());
         mEventLiveData.postValue(new Event(pkg, Event.EventType.RECEIVED));
     }
 
-    private void handleError(Exception err) {
+    private void handleError (Exception err){
         mEventLiveData.postValue(new Event(err));
     }
 
     @Override
-    public LiveData<Event> getOnEventChan() {
+    public LiveData<Event> getOnEventChan () {
         return mEventLiveData;
     }
 
     @Override
-    public boolean disconnect() {
+    public boolean disconnect () {
         return false;
     }
 
     @Override
-    public boolean isConnected() {
+    public boolean isConnected () {
         return false;
     }
 }
@@ -263,4 +173,74 @@ public class WifiController implements Communicator {
 class Statistics {
     public int mSentTotal = 0;
     public int mSentOK = 0;
+}
+
+class UnionSocket {
+    Socket mTcpSocket;
+    InputStream mInputStream;
+
+    DatagramSocket mUdpSocket;
+    boolean useUDP = false;
+    String mHost;
+    InetAddress mHostAddr;
+    int mPort;
+
+    private UnionSocket(String host, int port) {
+        mHost = host;
+        mPort = port;
+    }
+
+    public static UnionSocket createUDP(String host, int port) throws SocketException, UnknownHostException {
+        UnionSocket socket = new UnionSocket(host, port);
+        socket.mUdpSocket = new DatagramSocket(socket.mPort);
+        socket.mHostAddr = InetAddress.getByName(host);
+        socket.useUDP = true;
+        return socket;
+    }
+
+    public static UnionSocket createTCP(String host, int port) {
+        UnionSocket socket = new UnionSocket(host, port);
+        socket.mTcpSocket = new Socket();
+        return socket;
+    }
+
+    public void send(byte[] data) throws IOException {
+        if (useUDP) {
+            DatagramPacket pkg = new DatagramPacket(data, data.length, mHostAddr, mPort);
+            mUdpSocket.send(pkg);
+        } else {
+            mTcpSocket.getOutputStream().write(data);
+            mTcpSocket.getOutputStream().flush();
+        }
+    }
+
+    public int receive(byte[] buffer) throws IOException {
+        if (useUDP) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            mUdpSocket.receive(packet);
+            return packet.getLength();
+        } else {
+            return mInputStream.read(buffer);
+        }
+    }
+
+    public void newClient() throws IOException {
+        if (!useUDP) {
+            mInputStream = mTcpSocket.getInputStream();
+        }
+    }
+
+    public void connect() throws IOException {
+        if (!useUDP) {
+            mTcpSocket.connect(new InetSocketAddress(mHostAddr, mPort));
+        }
+    }
+
+    public boolean isActive() {
+        if (useUDP) {
+            return true;
+        } else {
+            return mTcpSocket.isConnected();
+        }
+    }
 }
